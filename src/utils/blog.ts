2ad30e13e07fe01d6ee64aefde721d61b40c8fa4
mtj0928 <mtj0928@users.noreply.github.com>
2026-01-18 15:28:36 +0900

    tmp

---
+++ src/utils/blog.ts
@@ -0,0 +1,16 @@
+import { getCollection } from 'astro:content';
+import type { CollectionEntry } from 'astro:content';
+
+type BlogPost = CollectionEntry<'blog'>;
+
+export async function getPublishedPosts(): Promise<BlogPost[]> {
+  const allPosts = await getCollection('blog');
+  return allPosts
+    .filter((post) => !post.data.draft)
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+}
+
+export async function getPublishedTags(posts?: BlogPost[]): Promise<string[]> {
+  const sourcePosts = posts ?? (await getPublishedPosts());
+  return [...new Set(sourcePosts.flatMap((post) => post.data.tags))].sort();
+}
